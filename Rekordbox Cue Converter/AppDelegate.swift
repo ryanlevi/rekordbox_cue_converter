c90b839b3e31a93fa5a7627923593bff0f082575
Ryan Levi <ryan.e.levi@gmail.com>
2017-12-24 20:56:52 -0800

    Initial Commit

---
+++ Rekordbox Cue Converter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rekordbox Cue Converter
+//
+//  Created by Ryan Levi on 12/24/17.
+//  Copyright © 2017 Ryan Levi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Rekordbox Cue Converter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Rekordbox Cue Converter
+//
+//  Created by Ryan Levi on 12/24/17.
+//  Copyright © 2017 Ryan Levi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
