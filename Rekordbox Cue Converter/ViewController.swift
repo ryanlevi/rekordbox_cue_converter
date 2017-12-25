cf60b1b0cbc758eb2e6bf27d15a0d34dfdbd0fe1
Ryan Levi <ryan.e.levi@gmail.com>
2017-12-24 22:22:43 -0800

    set up xcode project, created UI and linked outlets

--- Rekordbox Cue Converter/ViewController.swift
+++ Rekordbox Cue Converter/ViewController.swift
@@ -9,7 +9,20 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var filename_field: NSTextField!
+    @IBOutlet weak var convertOrCopy: NSComboBox!
+    @IBOutlet weak var from: NSComboBox!
+    @IBOutlet weak var to: NSComboBox!
+    @IBOutlet weak var colorA: NSColorWell!
+    @IBOutlet weak var colorB: NSColorWell!
+    @IBOutlet weak var colorC: NSColorWell!
+    @IBOutlet weak var colorD: NSColorWell!
+    @IBOutlet weak var colorE: NSColorWell!
+    @IBOutlet weak var colorF: NSColorWell!
+    @IBOutlet weak var colorG: NSColorWell!
+    @IBOutlet weak var colorH: NSColorWell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +34,40 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    
+    @IBAction func clickButton(_ sender: Any) {
+        let dialog = NSOpenPanel();
+        
+        dialog.title                   = "Choose your Rekordbox .xml file";
+        dialog.showsResizeIndicator    = true;
+        dialog.showsHiddenFiles        = false;
+        dialog.canChooseDirectories    = true;
+        dialog.canCreateDirectories    = true;
+        dialog.allowsMultipleSelection = false;
+        dialog.allowedFileTypes        = ["xml"];
+        
+        if (dialog.runModal() == NSModalResponseOK) {
+            let result = dialog.url // Pathname of the file
+            
+            if (result != nil) {
+                let path = result!.path
+                filename_field.stringValue = path
+            }
+        } else {
+            // User clicked on "Cancel"
+            return
+        }
+    }
+    @IBAction func submit(_ sender: Any) {
+        let file = filename_field
+        let cA = colorA.color
+        let cA_r:Int = Int(cA.redComponent * 256)
+        let cA_g:Int = Int(cA.greenComponent * 256)
+        let cA_b:Int = Int(cA.blueComponent * 256)
+        print(cA_r)
+        print(cA_g)
+        print(cA_b)
+    }
 
 }
 
